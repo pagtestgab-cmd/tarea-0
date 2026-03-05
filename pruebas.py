cef932124014442386d01cfdf546818f0848db0f
Gabriel de Jesús Azcorra Medina <pag.testgab@gmail.com>
2026-03-05 13:38:21 -0600

    Primer commit proyecto backend

--- pruebas.py
+++ pruebas.py
@@ -4,7 +4,7 @@ import requests
 # url = "http://127.0.0.1:8000/"
 
 # Remoto en Render
-url = "https://tarea-0-1-161w.onrender.com"
+url = "https://tarea-0-9j0h.onrender.com"
 
 
 respuesta = requests.get(url)
