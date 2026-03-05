7571284773bcf90723e6b976e2a038f020279b88
Gabriel de Jesús Azcorra Medina <pag.testgab@gmail.com>
2026-03-05 14:29:52 -0600

    Primer commit proyecto backend

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def inicio():
+    return {"respuesta": "Primer tarea realizada"}
+
 @app.get("/tarea-0")
 def tarea_0():
     return {"respuesta": "Primer tarea realizada"}
